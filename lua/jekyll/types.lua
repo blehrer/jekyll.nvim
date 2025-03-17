394a50c60133d60625236007383525f1eeb08d0a
Brian Lehrer <661570+blehrer@users.noreply.github.com>
2025-03-17 15:08:43 -0700

    define JekyllPlugin type, relocate declaration of M

---
+++ lua/jekyll/types.lua
@@ -0,0 +1,10 @@
+------
+---@class JekyllNvim
+---@field user_commands JekyllUserCommands commands that you can use from the : prompt
+---@field opts JekyllNvimOptions pluggable values
+---@field setup function(table<string,any>?) called to initialize the plugin
+---@field create_post function creates a jekyll post in _posts
+---@field create_draft function creates a jekyll post in _drafts
+---@field create_note function creates a jekyll note in _notes
+---@field promote_draft function converts an existing draft into a published post
+------
